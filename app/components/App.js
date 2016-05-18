5d389c885b477b8c0307d7a8dd10e7d49f505765
Tom Dalenberg <tom@dalenberg.nl>
2016-05-18 21:32:24 +0200

    setup react with hello world example

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+  chrome.bookmarks.getTree(function(tree) {
+    console.log(tree[0].children);
+  })
+
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import App from './components/App/App';
+import App from './components/App';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
-
-chrome.bookmarks.getTree(function(tree) {
-  console.log(tree[0].children);
-});
+render(<App />, document.getElementById('root'));
